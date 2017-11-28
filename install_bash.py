37fe1ddbda2e94bba412baa7b53b411dcc0a0242
Dennis Wulfert <dennis.wulfert@perfact.de>
2017-11-28 09:47:43 +0100

    Get install_bash running

--- install_bash.py
+++ install_bash.py
@@ -1,30 +1,39 @@
 # -*- coding: utf-8 -*-
 import os
-import random
-import urllib
-import urllib2
 
 PATH=os.path.dirname(os.path.abspath( __file__ )) # get current path and merge with imager path
-IMAGE_PATH_PARAM=" -i '" + PATH + "/no-country-for-old-men-3.jpg' "
-PARAM=" --expire-time=1 -t 20000 " # set params current param: set expire-time to 20 second
-MESSAGE=""
+BASH_PATH = PATH
+PARAM="NOTI.sh"
+MESSAGE="Ein Bash Script zum Ausführen des Scripts wurde erstellt"
 SUBJECT=" '→→Sprüche, Tipps und Philosophien←←' "
-print PATH
-def send_cmd(MESSAGE):
+BASH='python ' + '"' + BASH_PATH + '/run.py' + '"'
+print BASH
+def send_cmd(PATH, PARAM):
     '''
-    fetch command and
-    send command to terminal
+    fetch path from runner-script
+    touch File
+    open file and write bash command with path to run the run.py
     '''
-    print MESSAGE
-    os.system('notify-send ' + PARAM + IMAGE_PATH_PARAM + SUBJECT + MESSAGE )
+    runner_path = PATH + PARAM
+    os.system('touch "' + PATH + '"' + PARAM)
+    bash_in = open(runner_path)
+    bash_out = open(runner_path,"w")
+    bash_out.write(BASH)
+    bash_in.close()
+    bash_out.close()
 
-def create_bash(PATH):
+def create_bash(PATH, PARAM):
     '''
-    get length of data
-    choose random string from data
-    fetch it to a message together
-    TODO: crawl from website
+    get path and params
+    cut path to get path from /home/…/Schreibtisch/
     '''
-    send_cmd(MESSAGE=MESSAGE)
+    path_str = ''
+    PATH=PATH.split('/',4)
+    del_path = PATH[4]
+    PATH.remove(del_path)
+    for path_item in PATH:
+        path_str = path_str+path_item+'/'
+    PATH=path_str
+    send_cmd(PATH=PATH, PARAM=PARAM)
 
-create_bash(PATH=PATH)
+create_bash(PATH=PATH, PARAM=PARAM)
